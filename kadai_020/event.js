f1e357f336fcde271e2b43dff7df9e517805677c
ghitomichi <s05ta034@yahoo.co.jp>
2025-07-24 19:14:37 +0900

    kadai_020アップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const addBtn = document.getElementById('btn')
+
+const firstTxet = document.getElementById('text');
+
+addBtn.addEventListener ('click' , () => {
+
+  firstTxet.textContent = "ボタンをクリックしました" ;
+
+})
